4a6060b91b8b06baa1fd9c911137198821415fbc
leanderkretschmer <106610902+leanderkretschmer@users.noreply.github.com>
2025-11-25 12:12:00 +0100

    updated view hook

--- modified-files/lib/redmine_contacts/hooks/view_issues_hook.rb
+++ modified-files/lib/redmine_contacts/hooks/view_issues_hook.rb
@@ -2,6 +2,7 @@ module RedmineContacts
   module Hooks
     class ViewIssuesHook < Redmine::Hook::ViewListener
       render_on :view_issues_form_details_bottom, partial: 'issues/assigned_contact_inject'
+      render_on :view_issues_show_details_bottom, partial: 'issues/assigned_contact_show_inject'
     end
   end
 end
